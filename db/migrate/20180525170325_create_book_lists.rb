29cd098288f4427968c9f2bbf09a6f2ced1241f9
Rishabh <rishabhminocha99900@gmail.com>
2018-05-29 15:31:00 -0400

    Updates

--- db/migrate/20180525170325_create_book_lists.rb
+++ db/migrate/20180525170325_create_book_lists.rb
@@ -1,9 +1,10 @@
 class CreateBookLists < ActiveRecord::Migration[5.2]
   def change
     create_table :book_lists do |t|
-      t.references :term, foreign_key: true
-
+      t.integer :term_id, foreign_key: true # this is what I changed
+      t.references :term # initially it was over here
       t.timestamps
+
     end
   end
 end
