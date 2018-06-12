1d64388878d9dfc1dca300a8edbeaef56ebd8dd4
Rishabh <rishabhminocha99900@gmail.com>
2018-06-12 14:43:26 -0400

    Added comments

--- app/controllers/book_lists_controller.rb
+++ app/controllers/book_lists_controller.rb
@@ -6,12 +6,16 @@ class BookListsController < ApplicationController
   def index
     @book_lists = BookList.all
     string_generator()
-    file_generator()
-    # render plain: @some_string
-    return
-    #and return
+
+    render plain: @some_string
   end
 
+  # does the same as string_generator
+  # given book_list_id ( Integer )
+
+#
+
+# TODO clean up
   def string_generator
     @some_string = ""
     @book_lists.each { |book_list|
@@ -26,44 +30,18 @@ class BookListsController < ApplicationController
           @some_string += sprintf("%-9s", "") # ISBN INT
           @some_string += sprintf("%-24s", book.author) # author
           @some_string += sprintf("%-55s", book.title) # book title
-          @some_string += sprintf("%4.2f", book.price) # book price
+          @some_string += sprintf("%-5.2f", book.price) # book price
           @some_string += sprintf("%-10s", book.publisher)  # book publisher 
         }
-        
         @some_string += "\n"
       }
     }
-
-    #render ''
   end
 
+# 3) make this asynchronously
   def file_generator
     send_data(@some_string, :filename => "textfile.txt")
   end
-
-#This is not the thing needed
-def book_record_generator(s)
-  department = s[0...7]
-  course_number = s[7...13]
-  edition = s[13...45]
-  isbn = s[45...59]
-  isbn_integer = s[49...58]
-  author = s[59...83]
-  title = s[83...138]
-  price = s[138...144]
-  unused = s[144...153]
-  publisher = s[153...163]
-  to_add_in_file = "Department: " + department + "\n"
-  to_add_in_file += "Course Number: " + course_number + "\n"
-  to_add_in_file += "Edition: " + edition + "\n" 
-  to_add_in_file += "ISBN: " + isbn + "\n"
-  to_add_in_file += "ISBN (int only): " + isbn_integer + "\n" 
-  to_add_in_file += "Author: " + author + "\n" 
-  to_add_in_file +=  "Title: " + title + "\n" 
-  to_add_in_file += "Price: " + price + "\n" +
-  to_add_in_file += "Publisher: " + publisher
-  puts to_add_in_file
-end
 
   # GET /book_lists/1
   # GET /book_lists/1.json
